a70da7efcbda7b8449964dcf7f35376dd6fb091e
andrei <andrei@pop-os.localdomain>
2022-09-02 16:16:02 +0300

    Add card

--- pages/sendElement.tsx
+++ pages/sendElement.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import {Address} from "@elrondnetwork/erdjs/out"; 
 
+import { Button, CardContent, Typography } from '@material-ui/core';
+import Card from "@material-ui/core/Card";
+import CardMedia from '@mui/material/CardMedia/CardMedia';
+ 
+import {
+  EuiCard,
+  EuiIcon,
+  EuiFlexGroup,
+  EuiFlexItem,
+} from '@elastic/eui';
+
+
 function hex2a(hexx: String) {
     var hex = hexx.toString();//force conversion
     var str = '';
@@ -24,16 +36,46 @@ function sendElement( {record} : {record: String}) {
     // console.log(token_from);  
     let amount_from = parseInt(splt[4], 16);
     // console.log(amount_from);  
+    const amount_to_human = amount_to / ( Math.pow(10,18)) ;
+    const amount_from_human = amount_from / ( Math.pow(10,18)) ;
+ 
+     
 
   return (
-    <label>
-    <input type="checkbox"   />
-    ${wallet}
-    ${token_to}
-    ${amount_to}
-    ${token_from}
-    ${amount_from}
-    </label>
+    // <label>
+    // <input type="checkbox"   />
+    // ${wallet}
+    // ${token_to}
+    // ${amount_to}
+    // ${token_from}
+    // ${amount_from}
+    // </label>
+  
+  <div>
+
+
+<EuiFlexItem key={record}>
+      <EuiCard
+        title={`Swap ${amount_to_human} ${token_to} for ${amount_from_human} ${token_from}`}
+        description={`Offer made by ${wallet}`}
+     
+      />
+    </EuiFlexItem>
+
+
+  {/* <Button variant="contained"  >
+      <Card text-gray-800 style={{backgroundColor: "coral"}} >
+      <CardContent>
+        <Typography  gutterBottom variant="h5" component="h5"> You send: {amount_to_human} {token_to}</Typography>
+        <Typography gutterBottom variant="h5" component="h5"> You receive: {amount_from_human} {token_from}  </Typography>
+        From: {wallet}
+      </CardContent>
+    </Card>
+  </Button> */}
+ 
+</div>
+  
+  
   )
 }
 
