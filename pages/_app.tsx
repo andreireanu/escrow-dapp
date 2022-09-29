fb9d12599999f33fd7422dff73078700b5743ad1
andrei <andrei@pop-os.localdomain>
2022-09-29 15:36:09 +0300

    Vercel commit 9

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,13 @@ import {Provider as ReduxProvider} from "react-redux";
 import Notifications from "../components/Notifications";
 import {AuthContextProvider} from "@elrond-giants/erd-react-hooks";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+console.log(process.env.NEXT_PUBLIC_NETWORK_API_ADDRESS);
+console.log(process.env.NEXT_PUBLIC_NODE_ENV);
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <ReduxProvider store={store} >
-            <AuthContextProvider env={process.env.NODE_ENV === "production" ? "mainnet" : "devnet"}>
+            <AuthContextProvider env={process.env.NEXT_PUBLIC_NODE_ENV === "production" ? "mainnet" : "devnet"}>
                 <Component {...pageProps} />
                 <Notifications/>
             </AuthContextProvider>
